af4376022581ede773927e3bbc281466d0bbce1c
Olha36 <ipzm222_kov@student.ztu.edu.ua>
2023-10-20 22:46:18 +0300

    another variant

--- accordeon/script.js
+++ accordeon/script.js
@@ -1,7 +1,11 @@
-const tab = document.querySelectorAll('.tab');
-console.log(tab);
-const active = document.querySelector('.active');
+// const tab = document.querySelectorAll('.tab');
+// console.log(tab);
+// const active = document.querySelector('.active');
 
-tab.forEach(el => el.addEventListener('click', function() {
-    el.classList.toggle('active');
-}))
+// tab.forEach(el => el.addEventListener('click', function() {
+//     el.classList.toggle('active');
+// }))
+
+document.addEventListener('click', (e) =>  {
+    console.log(e.target.closest('.tab')?.classList.toggle('active'));
+})
